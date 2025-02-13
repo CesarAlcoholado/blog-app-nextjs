11693ddb054369eb72a0816da4e1172e4447d64c
Cesar <cesar.alcoholado@hotmail.com>
2025-02-13 16:39:50 -0300

    finished project

--- app/posts/[postid]/page.tsx
+++ app/posts/[postid]/page.tsx
@@ -1,4 +1,6 @@
-import { getSortedPostsData } from "@/lib/posts"
+import getFormattedDate from "@/lib/getFormattedDate";
+import { getPostData, getSortedPostsData } from "@/lib/posts"
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 export function GenerateMetaData({ params }: {params: {postId: string}}) {
@@ -28,7 +30,20 @@ export default async function Post({ params }: {params: {postId: string}}) {
     return notFound()
   }
 
+  const { title, date, contentHtml } = await getPostData(postId)
+
+  const pubDate = getFormattedDate(date)
+
   return (
-    <div>page</div>
-  )
+    <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+      <h1 className="text-3xl mt-4 mb-0">{title}</h1>
+      <p className="mt-0">{pubDate}</p>
+      <article>
+        <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        <p>
+          <Link href="/">← Back to home</Link>
+        </p>
+      </article>
+    </main>
+  );
 }
